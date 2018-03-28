ab7633912a4734bb104b6e4c3158c4f3db50faef
JESSE GURAM <S328155668@OCDSB>
2018-03-28 11:03:21 -0400

    jesse

--- main.py
+++ main.py
@@ -3,10 +3,6 @@
 # Jesse Guram
 # March 28, 2018
 
-if CtoF:
-    enter F
-else:
-    enter C
 
 
 def CtoF(C):
@@ -14,16 +10,27 @@ def CtoF(C):
     F = (1.8)*C+32
 
     return F
-temperature = int(input('Enter your temperature in Celsius: '))
-print(int(round(CtoF(temperature))))
-
-
 
 def FtoC(F):
     """convert from fahrenheit to celcius"""
     C = (0.55556)*(F-32)
 
     return C
-temperature = int(input('Enter your temperature in Fahrenheit:'))
-print(int(round(FtoC(temperature))))
+
+print('enter 1 for Celcius or 2 Fahrenheit:')
+x = int(input())
+if x == 1:
+    print(temperature1)
+elif x == 2:
+     print(temperature2)
+else:
+    print('neither')
+
+temperature1 = int(input('Enter your temperature in Fahrenheit:'))
+print(int(round(FtoC(temperature1))))
+
+temperature2 = int(input('Enter your temperature in Celsius: '))
+print(int(round(CtoF(temperature2))))
+
+
 
